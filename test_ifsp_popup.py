39107f1c00e97443a291758ed7f2214ac24c0761
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2025-06-02 11:34:26 +0600

    Update test_ifsp_popup.py

--- test_ifsp_popup.py
+++ test_ifsp_popup.py
@@ -3,15 +3,14 @@ from playwright.sync_api import Page, expect, sync_playwright
 
 
 def test_ifsp_popup(page: Page, const=None):
-    page.goto("https://qa03.therapbd.net/")
+    page.goto("https://sample.test.net/")
 
     page.locator('#loginName').type('rasha-cos')
-    page.locator('#password').type('therap321#')
-    page.locator('#providerCode').type('DDD-ND')
+    page.locator('#password').type('sampletest#')
     page.get_by_role("button").click()
 
     ######################### ifsp popup ###############################
-    page.goto("https://qa03.therapbd.net/ma/ifsp/plan/familyConcerns?ifspId=2242")
+    page.goto("https://sample.test.net/ma/ifsp/plan/familyConcerns?ifspId=2242")
     page.wait_for_timeout(3000)
     page.locator('#openBtn').click()
 
