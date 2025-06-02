7c44963c36142cb68bf93ae15a4bebcce5120134
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2025-06-02 11:33:41 +0600

    Update test_iframe_without_locator.py

--- test_iframe_without_locator.py
+++ test_iframe_without_locator.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Page, expect, sync_playwright
 
 
 def test_iframe_without_locator(page: Page, const=None):
-    page.goto("https://qa02.therapbd.net/")
+    page.goto("https://sample.test.net/")
 
     page.locator('#loginName').type('rasha-cos')
     page.locator('#password').type('therap321#')
@@ -11,14 +11,14 @@ def test_iframe_without_locator(page: Page, const=None):
     page.get_by_role("button").click()
 
     ################################### iframe(without locator) ###############################
-    page.goto("https://qa02.therapbd.net//ma/admin2/userForm")
+    page.goto("https://sample.test.net//ma/admin2/userForm")
     page.locator('#new-title-add').click()
     page.wait_for_timeout(300)
     page.frames[1].locator('#title').type('DDD-ND')
 
     #page.wait_for_timeout(5000)
 
-    page.goto("https://qa02.therapbd.net/ma/ifsp/plan/ifspInfo?ifspId=2122")
+    page.goto("https://sample.test.net/ma/ifsp/plan/ifspInfo?ifspId=2122")
     page.locator("text=Add New").click()
     page.wait_for_timeout(300)
     page.frames[1].locator("text=Add").click()
