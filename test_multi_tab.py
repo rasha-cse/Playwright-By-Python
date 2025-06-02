dbc5c82538f590a9ef14689a754d491cfed7e327
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2025-06-02 11:35:05 +0600

    Update test_multi_tab.py

--- test_multi_tab.py
+++ test_multi_tab.py
@@ -3,18 +3,17 @@ from playwright.sync_api import Page, expect, sync_playwright
 
 
 def test_multi_tab(page: Page, const=None):
-    page.goto("https://qa02.therapbd.net/")
+    page.goto("https://sample.test.net/")
 
     page.locator('#loginName').type('rasha')
-    page.locator('#password').type('therap321#')
-    page.locator('#providerCode').type('SQA-TH')
+    page.locator('#password').type('sample.test#')
     page.get_by_role("button").click()
 
     page.locator('"Agree"').click()
 
     ################################### multi/new tab ###############################
 
-    page.goto("https://qa02.therapbd.net/ma/ger/preview?formId=GER-SQANY-JDX2PV3GU5RNP")
+    page.goto("https://sample.test.net/ma/ger/preview?formId=JDX2PV3GU5RNP")
     page.locator("id=clientInfo").click()
     #page.wait_for_timeout(5000)
     with page.context.expect_page() as tab:
