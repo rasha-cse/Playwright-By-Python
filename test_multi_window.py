3670fc0f5a701aff0c79f8e205f83a12d2cc492d
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2025-06-02 11:35:52 +0600

    Update test_multi_window.py

--- test_multi_window.py
+++ test_multi_window.py
@@ -3,15 +3,14 @@ from playwright.sync_api import Page, expect, sync_playwright
 
 
 def test_multi_window(page: Page, const=None):
-    page.goto("https://qa02.therapbd.net/")
+    page.goto("https://sample.test.net/")
 
     page.locator('#loginName').type('rasha-da')
-    page.locator('#password').type('therap321#')
-    page.locator('#providerCode').type('DDD-ND')
+    page.locator('#password').type('sample.test#')
     page.get_by_role("button").click()
 
     ################################### multi/new window ###############################
-    page.goto("https://qa02.therapbd.net/ma/admin2/userForm?userId=2366033")
+    page.goto("https://sample.test.net/ma/admin2/userForm?userId=2366033")
     with page.context.expect_page() as window:
         page.locator("text=Set State Specific User Information").click()
     new_window = window.value
@@ -19,7 +18,7 @@ def test_multi_window(page: Page, const=None):
 
     page.wait_for_timeout(5000)
 
-    page.goto("https://qa02.therapbd.net/ma/nd/osp/osp?_flowId=nd-osp-flow&formId=OSP-DDDND-K5Y4QLSZL5RPM")
+    page.goto("https://sample.test.net/ma/nd/osp/osp?_flowId=flow&formId=K5Y4QLSZL5RPM")
     with page.context.expect_page() as window:
         page.locator('"Attach"').click()
     new_window = window.value
