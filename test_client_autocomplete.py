1c93370010f791a332eda4158d44c2708b7fe34e
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2025-06-02 11:37:31 +0600

    Update test_client_autocomplete.py

--- test_client_autocomplete.py
+++ test_client_autocomplete.py
@@ -3,15 +3,14 @@ from playwright.sync_api import Page, expect, sync_playwright
 
 
 def test_client_autocomplete(page: Page, const=None):
-    page.goto("https://qa02.therapbd.net/")
+    page.goto("https://sample.test.net/")
 
     page.locator('#loginName').type('rasha-cos')
-    page.locator('#password').type('therap321#')
-    page.locator('#providerCode').type('DDD-ND')
+    page.locator('#password').type('sample.test#')
     page.get_by_role("button").click()
 
     ######################### client autocomplete ###############################
-    page.goto("https://qa02.therapbd.net/ma/client/individualSearch?adminSearch=true")
+    page.goto("https://sample.test.net/ma/client/individualSearch?adminSearch=true")
     page.locator('#clientLookup').type('michael adkins')
     page.wait_for_timeout(3000)
     page.locator('#clientLookup').press('ArrowDown+Enter')
